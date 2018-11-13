c4473c2ae9ec04e07ca6b05d96096b580d690845
rayz2007 <rayz2007@uw.edu>
2018-11-12 19:39:51 -0800

    done

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-struct Quiz: Decodable{
+struct Quiz: Codable{
     let title: String
     let desc: String
     let questions: [Question]
 }
 
-struct Question: Decodable{
+struct Question: Codable{
     let text: String
     let answer: String
     let answers: [String]
@@ -23,11 +23,15 @@ struct Question: Decodable{
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    let defaults = UserDefaults.standard
+    var toStore: NSArray = []
     var jsonData: [Quiz]? = nil
     var elements: [String] = ["science", "marvel", "math"]
     var titles: [String] = []
     var descriptions: [String] = []
+    var jsonUrlString: String = UserDefaults.standard.string(forKey: "url") ?? "http://tednewardsandbox.site44.com/questions.json"
+    
+    var urlTextField: UITextField = UITextField()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return jsonData?.count ?? 0;
@@ -41,13 +45,33 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
         cell.descriptionLabel.text = jsonData?[indexPath.row].desc
         cell.cellLabel.text = jsonData?[indexPath.row].title
-        cell.cellImage.image = UIImage(named: elements[indexPath.row])
+        
+        cell.cellImage.image = UIImage(named: "default")
+        //check if title has icon if not use default.
+        for  i  in 0...elements.count-1{
+            if((cell.cellLabel.text?.lowercased().range(of: elements[i])) != nil){
+                cell.cellImage.image = UIImage(named: elements[i])
+            }
+        }
+        
         return cell
     }
     
     @IBAction func settingsPressed(_ sender: Any) {
-        let alert = UIAlertController(title: "Settings", message: "Settings go here.", preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        let alert = UIAlertController(title: "Settings", message: "Enter Data URL", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addTextField { (textField: UITextField) in
+            self.urlTextField = textField
+            self.urlTextField.placeholder = "Enter url here"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Check now", style: UIAlertActionStyle.default, handler:{
+            (act: UIAlertAction) in
+            if((self.urlTextField.text) != nil){
+                self.fetchJson(self.urlTextField.text!)
+            }
+        }))
         
         self.present(alert, animated: true, completion: nil)
     }
@@ -58,7 +82,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         tableView.tableFooterView = UIView()
         
-        fetchJson("http://tednewardsandbox.site44.com/questions.json")
+        //grabbing url fetched successfully last time
+        if((defaults.object(forKey: "url")) != nil){
+            self.jsonUrlString = defaults.object(forKey: "url") as! String
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if(Reachability.isConnectedToNetwork()){
+            if(jsonData == nil){
+                fetchJson(jsonUrlString)
+            }
+        }else{
+            let alert = UIAlertController(title: "No Internet", message: "Using local data", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            print("no internet, grabbing from user default");
+            let anyData = defaults.object(forKey: "quizData")
+            if(anyData == nil){
+                let alert = UIAlertController(title: "No Local Data", message: "Please connect to internet", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }else{
+                let data = anyData as! Data
+                do{
+                    let questions = try JSONDecoder().decode([Quiz].self, from: data)
+                    self.jsonData = questions
+                    for q in questions{
+                        self.titles.append(q.title)
+                        self.descriptions.append(q.desc)
+                    }
+                }catch{
+                    self.failDownloadAlert()
+                }
+                self.tableView.reloadData()
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -77,9 +136,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func fetchJson(_ fetchUrl: String){
+        
         guard let url = URL(string: fetchUrl) else {return}
         URLSession.shared.dataTask(with: url) { (data, res, err) in
-            guard let data = data else {return}
+            guard let data = data else {
+                self.failDownloadAlert()
+                return
+            }
+            self.defaults.set(fetchUrl, forKey: "url")
+            self.defaults.set(data, forKey:"quizData")
             do{
                 let questions = try JSONDecoder().decode([Quiz].self, from: data)
                 self.jsonData = questions
@@ -87,17 +152,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     self.titles.append(q.title)
                     self.descriptions.append(q.desc)
                 }
-            }catch let jsonErr{
-                print(jsonErr)
+            }catch{
+                self.failDownloadAlert()
             }
             
             DispatchQueue.main.async{
                 self.tableView.reloadData()
             }
-            
             }.resume()
     }
+    
+    func failDownloadAlert(){
+        let alert = UIAlertController(title: "Download Failed", message: "Please check internet/ data URL/ data format", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
-
-
